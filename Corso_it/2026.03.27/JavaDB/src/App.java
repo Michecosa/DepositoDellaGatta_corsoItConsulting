fddb5118a92a804941608171d6c38adc9f937be6
Michela <micheladellagatta1@gmail.com>
2026-03-27 17:05:13 +0100

    feat: enhance database connection output and query execution in App class

--- Corso_it/2026.03.27/JavaDB/src/App.java
+++ Corso_it/2026.03.27/JavaDB/src/App.java
@@ -9,14 +9,32 @@ public class App {
         try {
 
             Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connesso al database!");
+            System.out.println("Connesso al database!\n");
+            // conn.close();
+            // System.out.println("Configurazione JDBC ok!");
+
+            // 1. Creo lo Statement
+            Statement stmt = conn.createStatement();
+
+            // 2. Eseguo la query
+            String sql = "SELECT actor_id, first_name, last_name FROM actor LIMIT 10";
+            ResultSet rs = stmt.executeQuery(sql);
+
+
+            // 3. Itero sui risultati
+            while (rs.next()) {
+                int id = rs.getInt("actor_id");
+                String nome = rs.getString("first_name");
+                String cognome = rs.getString("last_name");
+
+                System.out.println(id + ": " + nome + " " + cognome);
+            }
+
             conn.close();
-            System.out.println("Configurazione JDBC ok!");
 
         } catch (SQLException e) {
 
             e.printStackTrace();
-            
         }
     }
 }
