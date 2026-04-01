fdaf34e170aadd435937261b794ed8e9e8836a17
Michela <micheladellagatta1@gmail.com>
2026-04-01 10:31:33 +0200

    feat: add ProvaPolimorfismo class with Persona and Pirata implementations for greeting management

---
+++ Corso_it/2026.04.01/Mattina/ProvaPolimorfismo.java
@@ -0,0 +1,49 @@
+// Classe base
+class Persona {
+  protected String nome;
+
+  public Persona (String nome) {
+    this.nome = nome;
+  }
+
+  public void saluta() {
+    System.out.println("Ciao, sono "+nome+"... e niente");
+  }
+}
+
+// Sottoclasse che eredita da Persona
+class Pirata extends Persona {
+  public Pirata(String nome) {
+    super(nome);
+  }
+
+  // POLIMORFISMO D'EREDITARIETÀ (Override del metodo saluta)
+  @Override
+  public void saluta() {
+    System.out.println("Sono il pirata "+nome);
+  }
+}
+
+// Terza classe per gestire il saluto
+class GestoreSaluti {
+  public static void eseguiSaluto(Persona p) {
+    p.saluta();
+  }
+}
+
+// Classe principale
+public class ProvaPolimorfismo {
+  public static void main(String[] args) {
+    Persona p1 = new Persona("Mario");
+    Persona p2 = new Persona("Luigi");
+
+    Pirata pirata = new Pirata("Barbossa");
+
+    System.out.println("\n--- GestoreSaluti per PERSONA ---");
+    GestoreSaluti.eseguiSaluto(p1);
+    GestoreSaluti.eseguiSaluto(p2);
+
+    System.out.println("\n--- GestoreSaluti per PIRATA ---");
+    GestoreSaluti.eseguiSaluto(pirata);
+  }
+}
