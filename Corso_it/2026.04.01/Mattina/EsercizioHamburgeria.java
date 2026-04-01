e7a4c5ab45ec131ae4f0e4ba865409dc0de52fb6
Michela <micheladellagatta1@gmail.com>
2026-04-01 10:47:52 +0200

    feat: add EsercizioHamburgeria class with Cibo and Hamburger family implementations

---
+++ Corso_it/2026.04.01/Mattina/EsercizioHamburgeria.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Cibo {}
+
+// Famiglia Hamburger
+class Hamburger extends Cibo {}
+
+class Cheeseburger extends Hamburger {}
+class VegBurger extends Hamburger {}
+class DoubleBacon extends Hamburger {}
+
+// Famiglia Dessert (Extra)
+class Dessert extends Cibo {}
+
+public class EsercizioHamburgeria {
+  public static void main(String[] args) {
+    
+  }
+}
