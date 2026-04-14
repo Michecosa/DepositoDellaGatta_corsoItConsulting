eb47979341417d2fb43c26d3152417eb3cd37d44
Michela <micheladellagatta1@gmail.com>
2026-04-14 14:43:17 +0200

    feat: implement JPA entity models for Utente, Todo, and Commento with relational mappings

---
+++ Corso_it/2026.04.14/Pomeriggio/todolistrelazionale/todolistrelazionale/src/main/java/com/example/todolistrelazionale/model/Todo.java
@@ -0,0 +1,31 @@
+package com.example.todolistrelazionale.model;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "todo")
+public class Todo {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String descrizione;
+
+  private boolean completato;
+
+  @ManyToOne
+  @JoinColumn(name = "utente_id", nullable = false)
+  private Utente utente;
+
+  @OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Commento> commenti;
+}
