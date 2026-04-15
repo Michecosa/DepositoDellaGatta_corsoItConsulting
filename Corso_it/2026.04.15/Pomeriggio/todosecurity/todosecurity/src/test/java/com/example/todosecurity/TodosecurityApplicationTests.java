03c71e835aa4c0497e0385499fec8a2494b41388
Michela <micheladellagatta1@gmail.com>
2026-04-15 17:33:48 +0200

    feat: initialize Spring Boot security project with Maven wrapper and standard configuration

---
+++ Corso_it/2026.04.15/Pomeriggio/todosecurity/todosecurity/src/test/java/com/example/todosecurity/TodosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
